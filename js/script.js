cb6aa594ca50cd004a75265b2522bdb014891834
michellylavinia <michellylaviniaa@gmail.com>
2026-04-02 21:01:05 +0700

    Add optional features (dark mode, sort, highlight)

--- js/script.js
+++ js/script.js
@@ -34,12 +34,18 @@ function update() {
 
   let total = 0;
 
+  transactions.sort((a, b) => b.amount - a.amount);
+  
   transactions.forEach(t => {
     total += t.amount;
 
     const li = document.createElement("li");
+    const isLarge = t.amount > 50000 ? "style='color:red;'" : "";
+
     li.innerHTML = `
-      ${t.name} - Rp ${t.amount.toLocaleString("id-ID")}
+      <span ${isLarge}>
+        ${t.name} - Rp ${t.amount.toLocaleString("id-ID")} (${t.category})
+      </span>
       <button onclick="deleteItem(${t.id})">X</button>
     `;
 
@@ -89,3 +95,19 @@ function updateChart() {
 }
 
 update();
+
+function toggleDark() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark");
+}
